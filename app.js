1322450ddaf1b853ce81de04a11d4e2c466a3b67
Masha <yawarakairairakku@gmail.com>
2025-05-17 11:27:49 +0300

    added middlewares

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const logger = require('./src/middlewares/logger');
+const errorHandler = require('./src/middlewares/error_handler');
+
+app.use(logger);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const moviesRouter = require('./src/routes/movies_routes');
@@ -10,6 +15,8 @@ const sessionsRouter = require('./src/routes/sessions_routes');
 app.use(express.json());
 app.use('/movies', moviesRouter);
 app.use('/sessions', sessionsRouter);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Сервер работает на порту ${PORT}`));
